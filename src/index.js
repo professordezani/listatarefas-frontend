7672cb2da10caeb2a784b8d6f8a633b79c6012df
Henrique Dezani <dezani@gmail.com>
2020-06-08 22:51:42 +0000

    Adicionar ThemProvider (Material-UI)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import orange from '@material-ui/core/colors/orange';
+import amber from '@material-ui/core/colors/amber';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: amber,
+  },  
+});
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>
+, document.getElementById('root'));
